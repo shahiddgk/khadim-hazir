20f4cb519233450741c78a38a637d886ce9be8ee
Asim Sajjad <asimsajjad15@gmail.com>
2023-05-31 12:32:19 +0500

    Update index.php

--- index.php
+++ index.php
@@ -53,8 +53,13 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
+	if ($_SERVER['SERVER_NAME'] == 'localhost') {
+		define('ENVIRONMENT', 'development');
+	} else {
+		define('ENVIRONMENT', 'production');
+	}
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
