3d9e15d5ec6371b6121ef6eb2343fa3a659c1529
Naveedullah <naveedullah968@gmial.com>
2023-06-07 00:11:39 -0700

    quick fixes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -715,28 +715,80 @@ class Api extends CI_Controller {
 	}
 
 	public function jobsListing(){
-		$employe_id = $this->input->post('user_id');
+		$employe_id = $this->input->post('employee_id');
 		// echo "last 10 jobs"; exit;
-		$user=$this->common_model->join_three_tab_where_rows("jobs.id as job_id , DISTINCT(users.id) as user_id, employer_id, categories.name, jobs.category_id, en_job_description,ar_job_description,ur_job_description,
+		$user=$this->common_model->join_three_tab_where_rows("DISTINCT(users.id) as employee_id, jobs.id job_id, employer_id, categories.name, ur_name, ar_name, jobs.category_id, en_job_description,ar_job_description,ur_job_description,
 		en_min_price, en_max_price,ar_min_price, ar_max_price, ur_min_price, ur_max_price, active", 
 		"jobs", "users", "on (jobs.category_id=users.category_id)", 
 		"categories", "on (categories.id=jobs.category_id)", 
 		array("jobs.active"=>"Y",  "users.id"=>$employe_id));
-		$data=$user->result();
+		// $data=$user->result();
 		if($user->num_rows()>0){
-			$result['data']=$data;
+			$data=$user->result();
+			foreach($data as $key=>$value){
+				$en_array[$key]['employee_id']=$value->user_id;
+				$en_array[$key]['job_id']=$value->job_id;
+				$en_array[$key]['employer_id']=$value->employer_id;
+				$en_array[$key]['name']=$value->name;
+				$en_array[$key]['category_id']=$value->category_id;
+				$en_array[$key]['en_job_description']=$value->en_job_description;
+				$en_array[$key]['en_min_price']=$value->en_min_price;
+				$en_array[$key]['en_max_price']=$value->en_max_price;
+				$en_array[$key]['active']=$value->active;
+	
+				$ur_array[$key]=$en_array[$key];
+				$ur_array[$key]['name']=$value->ur_name;
+				$ur_array[$key]['en_job_description']=$value->ur_job_description;
+				$ur_array[$key]['en_min_price']=$value->ur_min_price;
+				$ur_array[$key]['en_max_price']=$value->ur_max_price;
+	
+				$ar_array[$key]=$en_array[$key];
+				$ar_array[$key]['name']=$value->ar_name;
+				$ar_array[$key]['en_job_description']=$value->ar_job_description;
+				$ar_array[$key]['en_min_price']=$value->ar_min_price;
+				$ar_array[$key]['en_max_price']=$value->ar_max_price;
+			}
+			$result['data']['en'] = $en_array;
+			$result['data']['ur'] = $ur_array;
+			$result['data']['ar'] = $ar_array;
+			// $result['data']=$data;
 			$result['message']['success'] = true;
 			$result['message']['code']='500';
 			$result['message']['msg']='All active jobs';
 		}else{
 			// echo "last 10 jobs"; exit;
-			$user=$this->common_model->join_two_tab_where_limit_order("jobs.id as job_id , '$employe_id' as user_id, employer_id, categories.name, jobs.category_id, en_job_description,ar_job_description,ur_job_description,
+			$user=$this->common_model->join_two_tab_where_limit_order("'$employe_id' as employee_id, jobs.id job_id, employer_id, categories.name, ur_name, ar_name, jobs.category_id, en_job_description,ar_job_description,ur_job_description,
 			en_min_price, en_max_price,ar_min_price, ar_max_price, ur_min_price, ur_max_price, active", 
 			"jobs", "categories", "on (categories.id=jobs.category_id)", 
 			array("jobs.active"=>"Y"), "10", "jobs.id", "DSC");
 			$data=$user->result();
-
-			$result['data']=$data;
+			foreach($data as $key=>$value){
+				$en_array[$key]['user_id']=$value->user_id;
+				$en_array[$key]['job_id']=$value->job_id;
+				$en_array[$key]['employer_id']=$value->employer_id;
+				$en_array[$key]['name']=$value->name;
+				$en_array[$key]['category_id']=$value->category_id;
+				$en_array[$key]['en_job_description']=$value->en_job_description;
+				$en_array[$key]['en_min_price']=$value->en_min_price;
+				$en_array[$key]['en_max_price']=$value->en_max_price;
+				$en_array[$key]['active']=$value->active;
+	
+				$ur_array[$key]=$en_array[$key];
+				$ur_array[$key]['name']=$value->ur_name;
+				$ur_array[$key]['en_job_description']=$value->ur_job_description;
+				$ur_array[$key]['en_min_price']=$value->ur_min_price;
+				$ur_array[$key]['en_max_price']=$value->ur_max_price;
+	
+				$ar_array[$key]=$en_array[$key];
+				$ar_array[$key]['name']=$value->ar_name;
+				$ar_array[$key]['en_job_description']=$value->ar_job_description;
+				$ar_array[$key]['en_min_price']=$value->ar_min_price;
+				$ar_array[$key]['en_max_price']=$value->ar_max_price;
+			}
+			$result['data']['en'] = $en_array;
+			$result['data']['ur'] = $ur_array;
+			$result['data']['ar'] = $ar_array;
+			// $result['data']=$data;
 			$result['message']['success'] = true;
 			$result['message']['code']='500';
 			$result['message']['msg']='Previous 10 jobs listed';
@@ -913,10 +965,11 @@ class Api extends CI_Controller {
 	}
 
 	public function applyForAJob(){
-		$user_id=$this->input->post('user_id');
+		$user_id=$this->input->post('employe_id');
 		$job_id=$this->input->post('job_id');
 		$user=$this->common_model->select_where("*", "jobs", array("id"=>$job_id, "active"=>"Y"));
 		$data=$user->result_array();
+		// echo $user_id; exit;
 		// echo "<pre>"; print_r($data); exit;
 		if($user->num_rows()>0){
 			$job=$this->common_model->join_two_tab_where_simple("*", "jobs_applied", "users", "on (jobs_applied.employee_id=users.id)", array("job_id"=>$job_id, "user_type"=>"employee", "employee_id"=>$user_id));
